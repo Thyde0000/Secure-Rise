eae5b121c86e34163b4232c2a364ad22a49187d7
Erasto Jr Damian Astudillo <42245338+Thyde0000@users.noreply.github.com>
2023-09-22 12:22:19 -0700

    Delete SecureRise directory

--- SecureRise/src/main/java/com/example/securerise/repository/AlarmRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.securerise.repository;
-
-
-import com.example.securerise.entity.Alarm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-
-}
